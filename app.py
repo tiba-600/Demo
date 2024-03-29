eb512d6280ad4d3ec6a44d3cd5511ab4870f9b10
tiba-600 <tibaabba@student.arteveldehs.be>
2024-03-29 16:59:40 +0100

    desing is klaar

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Hier kun je de logica toevoegen om de gegevens van het formulier te verwerken,
-        # bijvoorbeeld het verzamelen van inloggegevens en het opslaan in een database.
-        # In dit voorbeeld zullen we eenvoudigweg de ingediende gegevens afdrukken.
-        username = request.form['username']
+        voornaam = request.form['voornaam']
+        achternaam = request.form['achternaam']
+        email = request.form['email']
         password = request.form['password']
-        print(f'Gebruikersnaam: {username}, Wachtwoord: {password}')
-        return 'Bedankt voor het indienen van uw gegevens.'
+        print(f'email: {email}, Wachtwoord: {password}, voornaam: {voornaam}, achternaam: {achternaam}')
+        return render_template('confirmation.html') 
 
     return render_template('index.html')
 
